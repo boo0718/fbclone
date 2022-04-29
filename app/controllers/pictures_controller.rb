8bc4e4942d83b6a1c515f60e81cb081cfad06065
miki takezawa <mikiaaamiki@yahoo.co.jp>
2022-04-29 18:36:17 +0900

    picture route controller 作成

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,2 +1,33 @@
 class PicturesController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def confirm
+  end
+
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:image,:image_cache,:content)
+  end
 end
